6d70f5672a27f1927272c7e91211138de0b14821
foooomio <foooomio@gmail.com>
2021-04-04 18:30:20 +0900

    Update kohhi-net

--- kohhi-net/check-users.ts
+++ kohhi-net/check-users.ts
@@ -8,11 +8,18 @@ const slack = new IncomingWebhook(process.env.KOHHI_NET_SLACK_WEBHOOK_URL!);
 (async () => {
   const timestamp = new Date().getUTCMilliseconds();
   const url = 'https://dynmap.kohhi.net/up/world/world/' + timestamp;
-  const { players } = await fetch(url).then((res) => res.json());
+
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error(`${res.status} ${res.statusText} ${res.url}`);
+  }
+
+  const { players } = await res.json();
 
   if (players.length) {
-    const names = players.map((player: any) => player.name).join(', ');
-    await slack.send('Playing: ' + names);
+    const names = players.map((player: any) => player.name);
+    await slack.send('Players: ' + names.join(' '));
   }
 })().catch(async (error: Error) => {
   await slack.send(`${error.name}: ${error.message}`);
